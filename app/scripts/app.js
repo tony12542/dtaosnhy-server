9f8b9d9207dd86b6cb21ef30ad1812e31495d7a1
林育陞 <linyusheng@36-231-80-110.dynamic-ip.hinet.net>
2015-07-12 22:27:45 +0800

    20150712

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,9 @@
  *
  * Main module of the application.
  */
-angular
-  .module('dashWebclientApp', [
+
+
+angular.module('dashWebclientApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -38,3 +39,4 @@ angular
         redirectTo: '/'
       });
   });
+
